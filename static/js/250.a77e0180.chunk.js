685b6f8d358a223d02014983e7e4f6919eb484df
owin66 <78900270+owin66@users.noreply.github.com>
2023-03-26 06:30:57 +0000

    Deploying to gh-pages from @ owin66/goit-react-hw-05-movies@361e77304b17aa17e4cd5311f70a7a0c621f0faf 🚀

--- static/js/250.a77e0180.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[250],{406:function(e,t,c){c.r(t);var i=c(184);t.default=function(){return(0,i.jsx)("div",{children:"cast"})}}}]);
-//# sourceMappingURL=250.a77e0180.chunk.js.map
